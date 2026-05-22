64b90cf8ef142885efd4a1377694cd80b8bf092f
han <b_wars@yahoo.com>
2026-05-23 00:51:37 +0800

    Add sc-300 quiz.

--- kotlincode/AzureQuizer/build.gradle.kts
+++ kotlincode/AzureQuizer/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     // Core Telemetry Infrastructure
     implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
     implementation("io.micrometer:micrometer-registry-otlp:latest.release")
+    implementation("io.opentelemetry.instrumentation:opentelemetry-ktor-3.0:2.18.1-alpha")
+    implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure:1.62.0")
 
     // testing
     testImplementation(kotlin("test"))
